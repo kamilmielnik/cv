301412848ead568483a47999066ce0a17263baae
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2017-07-27 21:39:30 +0200

    Upgrading dependencies

--- src/components/contact-info/index.js
+++ src/components/contact-info/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ContactInfoEntry from './contact-info-entry';
 
 const ContactInfo = ({ contactInfo }) => (
