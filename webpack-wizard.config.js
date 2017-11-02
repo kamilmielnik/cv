6222b207414287f9d48e4b942368fe23098bb415
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2017-11-02 22:57:22 +0100

    1.2.0

--- webpack-wizard.config.js
+++ webpack-wizard.config.js
@@ -1,27 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  isDev: process.env.NODE_ENV === 'development',
-  isProd: process.env.NODE_ENV === 'production',
   env: {
     NODE_ENV: JSON.stringify(process.env.NODE_ENV)
   },
   input: {
     favicon: path.resolve(__dirname, 'html', 'favicon-v2.ico'),
     html: path.resolve(__dirname, 'html', 'index.html'),
-    js: path.resolve(__dirname, 'src', 'index.js'),
-    jsDev: path.resolve(__dirname, 'src', 'index-dev.js'),
     modules: [
       path.resolve(__dirname, 'src'),
       path.resolve(__dirname, 'node_modules', 'sidebar')
-    ],
-    styles: path.resolve(__dirname, 'src', 'styles'),
-    stylesGlobals: 'globals.scss',
-  },
-  output: {
-    directory: path.resolve(__dirname, 'dist'),
-    css: 'styles.css',
-    html: 'index.html',
-    js: 'bundle.js'
+    ]
   }
 };
