910ba28c424c8ec2737b26f88fd234d69b38a2e8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-05-14 04:53:58 +0200

    1.3.1 - updating title & favicon

--- webpack-wizard.config.js
+++ webpack-wizard.config.js
@@ -1,6 +1,6 @@
 module.exports = (webpackWizard, { resolveCwdPath }) => ({
   input: {
-    favicon: resolveCwdPath('html/favicon-v2.ico'),
+    favicon: resolveCwdPath('html/favicon-v4.ico'),
     html: resolveCwdPath('html/index.html'),
     modules: [
       resolveCwdPath('src')
