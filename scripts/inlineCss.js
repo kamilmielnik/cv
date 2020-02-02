7566b7cee4364a3850595d04037b5ef5e16c9026
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-02-02 12:57:25 +0700

    Rename criticalCss script to inlineCss, update dependencies

--- scripts/inlineCss.js
+++ scripts/inlineCss.js
@@ -7,23 +7,23 @@ const logger = require('./logger');
 
 dotenv.config();
 
-const criticalCss = async () => {
-  logger.info('Critical CSS: reading index.html');
+const inlineCss = async () => {
+  logger.info('Inline CSS: reading index.html');
   const indexHtmlFilepath = path.join(process.env.DEPLOY_SOURCE_DIRECTORY, 'index.html');
   const html = fs.readFileSync(indexHtmlFilepath, 'utf-8');
-  logger.info('Critical CSS: parsing index.html');
+  logger.info('Inline CSS: parsing index.html');
   const $ = cheerio.load(html);
   const $stylesheet = $('link[rel=stylesheet][href$=css]');
   if ($stylesheet.length === 1) {
     const cssFilepath = path.join(process.env.DEPLOY_SOURCE_DIRECTORY, $stylesheet.attr('href'));
-    logger.info(`Critical CSS: stylesheet found: "${cssFilepath}"`);
+    logger.info(`Inline CSS: stylesheet found: "${cssFilepath}"`);
     const css = fs.readFileSync(cssFilepath, 'utf-8');
     $stylesheet.replaceWith(`<style>${css}</style>`);
-    logger.info('Critical CSS: writing index.html');
+    logger.info('Inline CSS: writing index.html');
     fs.writeFileSync(indexHtmlFilepath, $.html());
   } else {
-    logger.info('Critical CSS: stylesheet not found');
+    logger.info('Inline CSS: stylesheet not found');
   }
 };
 
-criticalCss();
+inlineCss();
