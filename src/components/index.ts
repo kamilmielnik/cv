6f6864fd3b98d02cb6c8781b6779936632a8ea83
Kamil Mielnik <kamil@kamilmielnik.com>
2023-04-04 20:49:15 +0700

    Get rid of section component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export { default as Button } from './Button';
 export { default as ContactInfo } from './ContactInfo';
 export { default as Experience } from './Experience';
 export { default as Page } from './Page';
-export { default as Section } from './Section';
 export { default as SvgIcon } from './SvgIcon';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-import { Button, ContactInfo, Experience, Page, Section } from 'components';
+import { Button, ContactInfo, Experience, Page } from 'components';
 import { contactInfo, description, education, name, workExperience } from 'data';
 import { download, github, print } from 'icons';
 import { useTrack } from 'tracking';
@@ -32,17 +32,12 @@ const Index = () => {
       <div className={styles.content}>
         <Page>
           <h1 className={styles.name}>{name}</h1>
-          <h2 className={styles.description}>{description}</h2>
-
+          <p className={styles.description}>{description}</p>
           <ContactInfo className={styles.contactInfo} contactInfo={contactInfo} />
-
-          <Section title="Work Experience">
-            <Experience experience={workExperience} />
-          </Section>
-
-          <Section title="Education">
-            <Experience experience={education} />
-          </Section>
+          <h2 className={styles.sectionTitle}>Work Experience</h2>
+          <Experience experience={workExperience} />
+          <h2 className={styles.sectionTitle}>Education</h2>
+          <Experience experience={education} />
         </Page>
 
         <div className={styles.buttons}>
