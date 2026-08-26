d1570a469e88d5e0aa59c97ff5dbf4cffcf6d510
Kamil Mielnik <kamil@kamilmielnik.com>
2026-08-27 01:31:12 +0700

    13 - Wait for the fonts before rendering the PDF

--- src/pdf.mjs
+++ src/pdf.mjs
@@ -17,6 +17,7 @@ async function createPdf(filepath, url) {
     const page = await browser.newPage();
     await page.setJavaScriptEnabled(false);
     await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.evaluate(() => document.fonts.ready);
     await page.pdf({ format: 'a4', path: temporaryFilepath });
   } finally {
     await browser.close();
