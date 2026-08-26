d5390d1f8ddb26ccfd9c050cc943afa66655d915
Kamil Mielnik <kamil@kamilmielnik.com>
2026-08-27 01:31:11 +0700

    12 - Render the PDF with JavaScript disabled instead of intercepting tracking requests

--- src/pdf.mjs
+++ src/pdf.mjs
@@ -15,7 +15,7 @@ async function createPdf(filepath, url) {
 
   try {
     const page = await browser.newPage();
-    await ignoreTracking(page);
+    await page.setJavaScriptEnabled(false);
     await page.goto(url, { waitUntil: 'networkidle0' });
     await page.pdf({ format: 'a4', path: temporaryFilepath });
   } finally {
@@ -23,15 +23,4 @@ async function createPdf(filepath, url) {
   }
 
   await fs.rename(temporaryFilepath, filepath);
-}
-
-async function ignoreTracking(page) {
-  await page.setRequestInterception(true);
-  page.on('request', (request) => {
-    if (new URL(request.url()).pathname.startsWith('/track/')) {
-      request.abort();
-    } else {
-      request.continue();
-    }
-  });
 }
